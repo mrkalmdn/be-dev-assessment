20af54b1fddacd485f3801c4a6053f9837c281cc
Mark Kevin Almadin <mrkalmdn@gmail.com>
2025-12-02 00:52:33 +0800

    feat: Add URL model, migration, and factory

---
+++ url-shortener/app/Models/Url.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Url extends Model
+{
+    /** @use HasFactory<\Database\Factories\UrlFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'original_url',
+        'short_url',
+        'click_count',
+    ];
+
+    protected $casts = [
+        'click_count' => 'integer',
+    ];
+}
